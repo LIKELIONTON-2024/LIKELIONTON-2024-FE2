1adf77ead03a95fdd567dcc20eac69580feee5ba
moko <ljun925@naver.com>
2024-07-25 19:24:44 +0900

    feat: login 스크린 구현

---
+++ src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import {SafeAreaView, Text, View} from 'react-native';
+import {COLOR} from '../module/Color';
+import React from 'react';
+import {Button} from '../components/common/button';
+import GoogleLoginButton from '../components/GoogleLoginButton';
+
+export default ({navigation}) => {
+  const onPresskakaoBtn = () => {
+    navigation.navigate('Agree');
+  };
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLOR.BLUE_400,
+        alignItems: 'center',
+      }}>
+      <View
+        style={{
+          flex: 1,
+          gap: 8,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{
+            alignSelf: 'flex-start',
+            color: COLOR.WHITE,
+            fontWeight: 'bold',
+          }}>
+          나가서 바람이라도 쐐____________
+        </Text>
+        <Text style={{fontWeight: 'bold', fontSize: 50}}>힉힉호무리</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 14,
+        }}>
+        <Button
+          onPress={onPresskakaoBtn}
+          bgColor={COLOR.KAKAOBUTTON}
+          text={'카카오로 계속하기'}
+        />
+        <Button
+          onPress={() => {}}
+          bgColor={COLOR.NAVERBUTTON}
+          text={'네이버로 계속하기'}
+        />
+        <GoogleLoginButton />
+      </View>
+    </SafeAreaView>
+  );
+};
